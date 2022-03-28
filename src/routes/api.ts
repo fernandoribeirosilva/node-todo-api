d02692858ad007b5f68460a27f8964adb0e440a2
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-03-27 22:15:45 -0300

    primeiro commit

---
+++ src/routes/api.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as ApiController from "../controller/apiControler";
+
+const router = Router();
+
+router.get('/ping', ApiController.ping);
+router.get('/random', ApiController.random);
+router.get('/nome/:nome', ApiController.nome);
+
+
+export default router;
